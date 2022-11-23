76ad26d2db1ce603c7ed94dca4364ee53bef1c1e
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-24 00:33:49 +0300

    Reversed test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,4 +9,13 @@ describe Solver do
       expect(solver.factorial(7)).to eq(5040)
     end
   end
+
+  context 'Testing Solver Class' do
+    it 'Should return a reversed string' do
+      solver = Solver.new
+      expect(solver.reverse('hello')).to eq('olleh')
+      expect(solver.reverse('world')).to eq('dlrow')
+      expect(solver.reverse('ruby')).to eq('ybur')
+    end
+  end
 end
