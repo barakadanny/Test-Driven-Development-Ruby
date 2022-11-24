1f4895ff6bae526f17ba5a269036324d3547891e
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-24 13:18:38 +0300

    Test exceptions

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,6 +4,9 @@ describe Solver do
   context 'Testing Solver Class' do
     it 'Should return the factorial of number passed to factorial method' do
       solver = Solver.new
+      expect(solver.factorial(0)).to eq(1)
+      expect { @solver.factorial(-5) }.to raise_error
+      expect(solver.factorial(1)).to eq(1)
       expect(solver.factorial(5)).to eq(120)
       expect(solver.factorial(6)).to eq(720)
       expect(solver.factorial(7)).to eq(5040)
